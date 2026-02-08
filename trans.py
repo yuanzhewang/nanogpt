90ef7f98ac928f783c08dea1b7b9b80a2076e5d7
Yuanzhe Wang <yuanzhewangtju@gmail.com>
2026-02-07 23:55:46 -0800

    Complete the first round of Transformer model.

--- trans.py
+++ trans.py
@@ -142,7 +142,7 @@ class CausalSelfAttention(torch.nn.Module):
         self.AttnDropout = torch.nn.Dropout(p=dropout)
         self.OutputDropout = torch.nn.Dropout(p=dropout)
     
-    def forward(self, x, targets=None):
+    def forward(self, x):
         # Parameters
         B, T, C = x.shape
         N = self.num_heads
@@ -179,50 +179,87 @@ class CausalSelfAttention(torch.nn.Module):
         # Output dropout
         x = self.OutputDropout(x)
         return x
-        
+
+class FeedForward(torch.nn.Module):
+    def __init__(self, num_embed, dropout) -> None:
+        super().__init__()
+        self.W1 = torch.nn.Linear(num_embed, num_embed * 4, bias=True)
+        self.GELU = torch.nn.GELU()
+        self.W2 = torch.nn.Linear(num_embed * 4, num_embed, bias=True)
+        self.Dropout = torch.nn.Dropout(p = dropout)
+
+    def forward(self, x):
+        x = self.W1(x)
+        x = self.GELU(x)
+        x = self.W2(x)
+        x = self.Dropout(x)
+        return x
+
+class Block(torch.nn.Module):
+    def __init__(self, block_size, num_embed, num_heads, dropout) -> None:
+        super().__init__()
+        self.LN1 = torch.nn.LayerNorm(num_embed)
+        self.LN2 = torch.nn.LayerNorm(num_embed)
+        self.Attn = CausalSelfAttention(block_size, num_embed, num_heads, dropout)
+        self.FF = FeedForward(num_embed, dropout)
+
+    def forward(self, x):
+        x = x + self.Attn(self.LN1(x))
+        x = x + self.FF(self.LN2(x))
+        return x
+
 
 class TransformerModel(torch.nn.Module):
     def __init__(self, vocab_size, block_size, num_embed, num_heads, num_layers, dropout) -> None:
         super().__init__()
-
         self.embedding = torch.nn.Embedding(vocab_size, num_embed)
         self.pos_embedding = torch.nn.Embedding(block_size, num_embed)
+        self.blocks = torch.nn.Sequential(*[Block(block_size, num_embed, num_heads, dropout) for i in range(num_layers)])
+        self.Dropout = torch.nn.Dropout(p=dropout)
+        self.LNf = torch.nn.LayerNorm(num_embed)
+        self.Linf = torch.nn.Linear(num_embed, vocab_size, bias=False)
 
     def forward(self, x, targets=None):
-        logits = self.embedding(x)
+        B, T = x.shape
+        x = self.embedding(x) + self.pos_embedding(torch.arange(0, T, dtype=torch.long, device=x.device))
+        x = self.Dropout(x)
+        x = self.blocks(x)
+        x = self.LNf(x)
+        logits = self.Linf(x)
         loss = None
-        B, T, C = logits.shape
+        
         if targets is not None:
-            loss = torch.nn.functional.cross_entropy(logits.view(B * T, C), targets.view(B * T))
+            loss = torch.nn.functional.cross_entropy(logits.view(B * T, -1), targets.view(B * T))
         return logits, loss
 
 #####################################
 # Training Loop
 #####################################
 
-bigram_model = BigramModel(vocab_size=vocab_size)
-bigram_model.to(device)
+# model = BigramModel(vocab_size=vocab_size)
+model = TransformerModel(vocab_size, block_size, num_embed, num_heads, num_layers, dropout)
+model.to(device)
 
-optimizer = torch.optim.AdamW(bigram_model.parameters(), lr=learning_rate)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 time_start = time.time()
 
 def validate():
-    bigram_model.eval()
+    model.eval()
     loss_sum = 0.0
     with torch.no_grad():
         for _ in range(eval_examples):
             x, y = get_batch('validation')
-            _, loss = bigram_model(x, y)
+            _, loss = model(x, y)
             loss_sum += loss.item()
-    bigram_model.train()
+    model.train()
     return loss_sum / eval_examples
 
 for s in range(training_steps):
     # 1. Get data
     x, y = get_batch('training')
     # 2. Forward
-    logits, loss = bigram_model(x, y)
+    logits, loss = model(x, y)
     # 3. Validation
     if s % eval_interval == 0 or s == training_steps - 1:
         validation_loss = validate()
@@ -243,12 +280,12 @@ for s in range(training_steps):
 idx = torch.zeros((inference_num, 1), dtype=torch.long, device=device)
 
 # Model to evaluation mode.
-bigram_model.eval()
+model.eval()
 
 with torch.no_grad():
     for step in range(inference_length):
         # 1. Generate
-        logits, _ = bigram_model(idx)
+        logits, _ = model(idx[:, -block_size:])
         # 2. Logits -> Probs
         probs = torch.nn.functional.softmax(logits[:, -1, :], dim=-1)
         # 3. Sample
@@ -261,4 +298,4 @@ for i_example, example in enumerate(idx):
     print(f'Generation [{i_example}]: {gen_text}\n\n')
 
 # In case we need to train model later on in the script.
-bigram_model.train()
+model.train()
